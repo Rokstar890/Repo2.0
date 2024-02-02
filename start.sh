e0c93bdebba90dd9657a5da9a1284e44cead3fa1
Rokstar890 <144723163+Rokstar890@users.noreply.github.com>
2024-02-02 11:26:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,13 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/HarshalPurohitEdits/TheMovieProviderBot.git /TheMovieProviderBot 
+  git clone https://github.com/rokstar890/Repo2.0.git /Repo2.0 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TheMoviesProviderBot
+  git clone $UPSTREAM_REPO /
+  Repo2.0
 fi
 cd /THALAPATHY-FILTER-BOT 
 pip3 install -U -r requirements.txt
-echo "Starting TheMovieProviderBot...."
+echo "Starting Repo2.0...."
 python3 bot.py
